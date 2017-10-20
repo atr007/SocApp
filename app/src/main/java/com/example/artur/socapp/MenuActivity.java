c2e82bb126da2e50784653b06a57839325520746
atr007 <vav-94@yandex.ru>
2017-10-20 23:57:22 +0300

    Third commit

--- app/src/main/java/com/example/artur/socapp/MenuActivity.java
+++ app/src/main/java/com/example/artur/socapp/MenuActivity.java
@@ -1,13 +1,26 @@
 package com.example.artur.socapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
+
+
+    private Button mCameraButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        mCameraButton = (Button)findViewById(R.id.cameraButton);
+    }
+
+    public void onCameraButtonClick(View view){
+        Intent intent = new Intent(MenuActivity.this, CameraActivity.class);
+        startActivity(intent);
     }
 }

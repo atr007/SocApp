92d216f2c39571b487047132b9ca49e976db8c59
atr007 <vav-94@yandex.ru>
2017-10-24 18:57:57 +0300

    5 commit

--- app/src/main/java/com/example/artur/socapp/StartActivity.java
+++ app/src/main/java/com/example/artur/socapp/StartActivity.java
@@ -5,6 +5,9 @@ import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.vk.sdk.VKSdk;
+import com.vk.sdk.VKUIHelper;
+
 public class StartActivity extends AppCompatActivity {
 
     private int mRate = 1000;
@@ -16,6 +19,8 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        //VKSdk.initialize(this);
     }
 
     public void onStart(){
